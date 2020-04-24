9477e2fcf68b9b24434d175b9888b8534061f700
Guillaume Briday <guillaumebriday@gmail.com>
2020-04-24 20:46:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<template>
+<div class="relative bg-gray-50 py-20 px-5">
+  <div class="relative max-w-6xl mx-auto">
+    <div class="grid gap-5 mx-auto md:grid-cols-2 lg:grid-cols-3">
+        <Card
+          v-for="project in projects"
+          :project="project"
+          :user="user"
+          :key="project.name"
+        />
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import Card from './components/Card'
+
+export default {
+  components: {
+    Card
+  },
+
+  data () {
+    return {
+      user: 'guillaumebriday',
+      projects: [
+        {
+          name: 'laravel-blog',
+          travis: 'laravel-blog'
+        },
+        { name: 'traefik-docker-ansible' },
+        {
+          name: 'todolist-backend-laravel',
+          gitlab: 'todolist-backend-laravel'
+        },
+        {
+          name: 'todolist-frontend-vuejs',
+          netlify: '68286221-cb5f-4960-834f-0ae250395090',
+          travis: 'todolist-frontend-vuejs'
+        },
+        {
+          name: 'vue-scroll-progress-bar',
+          npm: '@guillaumebriday/vue-scroll-progress-bar',
+          netlify: '63086bf9-f804-451e-9319-052191432063'
+        },
+        {
+          name: 'jsonapi-scopes',
+          gem: 'jsonapi-scopes'
+        },
+        {
+          name: 'custom-element-password-toggle',
+          npm: 'custom-element-password-toggle',
+          netlify: 'f48191db-a459-4ab4-849f-10ea970915af',
+          workflows: [
+            'Test',
+            'Lint'
+          ]
+        },
+        {
+          name: 'guillaumebriday.fr',
+          netlify: 'f1ff9daf-4123-4b27-9aab-761073812859'
+        },
+        {
+          name: 'json-api-response-converter',
+          npm: 'json-api-response-converter'
+        },
+        {
+          name: 'webpack-boilerplate',
+          netlify: 'b4f7398b-780c-42e8-8a3e-080c497fda4e'
+        },
+        {
+          name: 'docker-ruby-node',
+          docker: 'guillaumebriday/ruby-node'
+        },
+        {
+          name: 'todolist-frontend-nuxt',
+          netlify: '61117a85-2c1d-4308-be10-3c77413e8a74',
+          travis: 'todolist-frontend-nuxt'
+        },
+        {
+          name: 'traefik-custom-error-pages',
+          docker: 'guillaumebriday/traefik-custom-error-pages',
+          netlify: '64de9cea-fa16-4f76-b5b8-a1abb5eb4e2f'
+        },
+        {
+          name: 'youarenotfacebook',
+          netlify: '80093602-8732-45c2-bbb2-3eae48ba6632'
+        },
+        {
+          name: 'lobsters-docker',
+          docker: 'guillaumebriday/lobsters-docker'
+        },
+        {
+          name: 'rails-vue-cli',
+          docker: 'guillaumebriday/rails-vue-cli'
+        },
+        { name: 'selfhosted-services' },
+        { name: 'rails-tailwindcss-starter' },
+        { name: 'traefik-file-config-example' },
+        { name: 'insomnia-telegram-workspace' },
+        { name: 'homelab-docker-ansible' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  @apply bg-gray-200;
+}
+</style>
+
+<style src="./assets/tailwind.css">
---
+++ src/components/Card.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+    <div class="flex-1 bg-white p-6">
+      <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
+        <a :href="`https://github.com/${user}/${repo}`" class="flex items-center">
+
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            class="mr-2 w-4 h-4"
+          >
+            <path
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+          {{ project.name }}
+        </a>
+      </h3>
+
+      <p class="mt-3">
+        <img v-for="src in github" :src="src" :key="src" class="badge" />
+        <img v-for="src in workflows" :src="src" :key="src" class="badge" />
+        <img v-for="src in npm" :src="src" :key="src" class="badge" />
+        <img v-for="src in gems" :src="src" :key="src" class="badge" />
+        <img v-for="src in docker" :src="src" :key="src" class="badge" />
+
+        <img v-if="project.gitlab" :src="`https://gitlab.com/${user}/${project.gitlab}/badges/master/pipeline.svg`" class="badge" />
+        <img v-if="project.travis" :src="`https://travis-ci.org/${user}/${project.travis}.svg?branch=master`" class="badge" />
+        <img v-if="project.netlify" :src="`https://img.shields.io/netlify/${project.netlify}`" class="badge" />
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: String,
+      required: true
+    },
+
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    repo () {
+      const name = this.project.github || this.project.name
+
+      return `${this.user}/${name}`
+    },
+
+    workflows () {
+      if (!this.project.workflows) return []
+
+      return this.project.workflows.map(workflow => `https://github.com/${this.repo}/workflows/${workflow}/badge.svg`)
+    },
+
+    github () { return this.buildBadges('github', ['stars', 'forks', 'issues', 'issues-pr', 'license'], this.repo) },
+    npm () { return this.buildBadges('npm', ['v', 'dt'], this.project.npm) },
+    gems () { return this.buildBadges('gem', ['v', 'dt'], this.project.gem) },
+    docker () { return this.buildBadges('docker', ['pulls', 'stars'], this.project.docker) }
+  },
+
+  methods: {
+    buildBadges (service, badges, repository) {
+      if (!repository) return []
+
+      return badges.map(badge => `https://img.shields.io/${service}/${badge}/${repository}.svg`)
+    }
+  }
+}
+</script>
+
+<style>
+.badge {
+  @apply inline-block ml-2 mb-2;
+}
+</style>
